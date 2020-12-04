f73c85c6a8c92f6d8b9ae590fd57cba3d01112f9
JoseMRodriguezLozano <jose.rodriguez94@upr.edu>
2020-12-04 13:33:10 -0400

    Update about_us_dao.py

--- OdinAPI/handler/dao/about_us_dao.py
+++ OdinAPI/handler/dao/about_us_dao.py
@@ -19,18 +19,19 @@ class AboutUsDAO:
     def addAboutUs(self, hdmember, title, major, picture, htype):
         """
         Summary:
-            Creates a multimedia post with the given title, content, type of multimedia, and author,
-            and inserts it into the database returning the id of the newly created multimedia post if succesful,
+            Creates a aboutus record with the given title, content, type of AboutUs, and author,
+            and inserts it into the database returning the id of the newly created an aboutus record if succesful,
             or an error if otherwise.
 
         Params:
-            title: the title of the multimedia post.
-            content: the content of the post.
-            mType: the type of multimedia post.
-            duid: the dashboard user id of the author of the multimedia post.
+            title: the position in the directive among Huella members
+            hdmember: Member of the directive of Huella
+            htype: the type of Aboutus record.
+            picture: profile picture of the member.
+            major: Study concentration.
 
         Returns:
-            The id of the newly added multimedia post
+            The id of the newly added record
         """
         cursor = self.conn.cursor() 
 
@@ -45,27 +46,27 @@ class AboutUsDAO:
             hdID = cursor.fetchone()[0]
             cursor.close()
             if not hdID:
-                return "Occurrió un error interno tratando de añadir una publicación multimedia."       
+                return "Occurrió un error interno tratando de añadir un aboutus record."       
         except psycopg2.DatabaseError as e:
             print(e)
-            return "Occurrió un error interno tratando de añadir una publicación multimedia."
+            return "Occurrió un error interno tratando de añadir un aboutus record."
         
         #Commits the changes done on the database after insertion
         try:
             self._commitChanges()                   
         except:
-            return "Occurrió un error interno tratando de añadir una publicación multimedia."
+            return "Occurrió un error interno tratando de añadir un aboutus record."
         
-        #Return id of the newly created multimedia post
+        #Return id of the newly created aboutus record
         return hdID
 
     def getAllAboutUs(self):
         """
         Summary:
-            Returns a list of all multimedia posts that are valid in the database with their corresponding information.
+            Returns a list of all aboutus records that are valid in the database with their corresponding information.
 
         Returns:
-            A list containing all the valid multimedia posts with their information.
+            A list containing all the valid aboutus records with their information.
         """
 
         cursor = self.conn.cursor()
@@ -84,19 +85,19 @@ class AboutUsDAO:
             return result
         except psycopg2.DatabaseError as e:
             print(e)
-            return "Ocurrió un error interno buscando todas las publicaciones multimedia."
+            return "Ocurrió un error interno buscando todos los records de aboutus"
 
     def getAboutUsByID(self, hdID):
         """
         Summary:
-            Returns a single multimedia post that is valid in the database with their corresponding information
-            with the given multimedia post id.
+            Returns a single aboutus record that is valid in the database with their corresponding information
+            with the given aboutus id.
 
         Params:
-            mID: the id of the multimedia post id to be fetched.
+            hdID: the id of the aboutus to be fetched.
 
         Returns:
-            A list containing all the information of the valid multimedia post with the given multimedia post id.
+            A list containing all the information of the valid aboutus with the given aboutus record id.
         """
         
         cursor = self.conn.cursor()
@@ -113,19 +114,19 @@ class AboutUsDAO:
             return result
         except psycopg2.DatabaseError as e:
             print(e)
-            return "Ocurrió un error interno buscando una publicación multimedia por su identificador."
+            return "Ocurrió un error interno buscando un record de aboutus por su identificador."
     
     def getAboutUsByType(self, hType):
         """
         Summary:
-            Returns a a list of multimedia posts that are valid in the database with their corresponding information
+            Returns a a list of aboutus records that are valid in the database with their corresponding information
             and are of the given type.
 
         Params:
-            mType: the type of multimedia post.
+            hType: the type of aboutus record.
 
         Returns:
-            A list containing all the valid multimedia posts with their information and are of the given type.
+            A list containing all the valid aboutus records with their information and are of the given type.
         """
 
         cursor = self.conn.cursor()
@@ -145,23 +146,23 @@ class AboutUsDAO:
             return result
         except psycopg2.DatabaseError as e:
             print(e)
-            return "Occurrió un error interno buscando publicaciones del tipo de multimedia dado." 
+            return "Occurrió un error interno buscando publicaciones del tipo de aboutus dado." 
 
     def editAboutUs(self, hdID, hdmember, title, major, picture):
         """
         Summary:
-            Updates an existing multimedia post in the database using the given title, content, type of multimedia, 
-            and author, returning the id of the newly updated multimedia post if succesful, or an error if otherwise.
+            Updates an existing aboutus record in the database using the given title, major, type of aboutus record, 
+            and hdmember, returning the id of the updated aboutus if succesful, or an error if otherwise.
 
         Params:
-            mID: the id of the multimedia post to be updated.
-            title: the title of the multimedia post.
-            content: the content of the post.
-            mType: the type of multimedia post.
-            duid: the dashboard user id of the author of the multimedia post.
+            hID: the id of the aboutus to be updated.
+            title: Position of the Huella member.
+            major: Study concentration of the member.
+            hType: the type of aboutus record.
+            hdmember: Member of directiva of Huella
 
         Returns:
-            The id of the newly updated multimedia post.
+            The id of the newly updated AboutUs record.
         """
 
         cursor = self.conn.cursor()
@@ -181,30 +182,30 @@ class AboutUsDAO:
             cursor.execute(query,(hdmember, title, major, picture, hdID,))
             result = cursor.fetchone()
             if not result:
-                return "Occurrió un error interno editando una publicación multimedia existente."
+                return "Occurrió un error interno editando un record de aboutus existente."
         except Exception as e:
             print(e)
-            return "Ocurrió un error interno editando una publicación multimedia existente."             
+            return "Ocurrió un error interno editando unrecord de aboutus existente."             
         
         try:
             self._commitChanges()                  
         except:
-            return "Ocurrió un error interno editando una publicación multimedia existente."            
+            return "Ocurrió un error interno editando un aboutus existente."            
        
-        #Return id of the newly updated multimedia post
+        #Return id of the newly updated aboutus
         return result[0] 
     
     def removeAboutUs(self, hdID):
         """
         Summary:
-            Sets as invalid a multimedia post in the database with the given multimedia post id.
-            This effectively acts as a removal of the multimedia post from the system.
+            Sets as invalid a aboutus record in the database with the given aboutus id.
+            This effectively acts as a removal of the aboutus from the system.
 
         Params:
-            mID: The id of the multimedia post to invalidate.
+            hdID: The id of the aboutus record to invalidate.
 
         Returns:
-            The id of the updated and invalid multimedia post.
+            The id of the updated and invalid aboutus.
         """
         
         cursor = self.conn.cursor()
@@ -221,29 +222,29 @@ class AboutUsDAO:
             cursor.execute(query, (hdID, ))
             result = cursor.fetchone()
             if not result:
-                return "Occurrió un error interno removiendo una publicación multimedia existente."
+                return "Occurrió un error interno removiendo un record de AboutUs existente."
         except Exception as e:
             print(e)
-            return "Ocurrió un error interno removiendo una publicación multimedia existente."             
+            return "Ocurrió un error interno removiendo un record de AboutUs existente."             
         
         try:
             self._commitChanges()                  
         except:
-            return "Ocurrió un error interno removiendo una publicación multimedia existente."  
+            return "Ocurrió un error interno removiendo un record de AboutUs existente."  
 
-        #Return id of the newly updated multimedia post
+        #Return id of the newly updated aboutus record
         return result[0] 
 
     def aboutUsExists(self, hdID):
         """
         Summary:
-            Confirms the existence of a multimedia post in the database by the multimedia post id given.
+            Confirms the existence of am aboutus in the database by the aboutus id given.
 
         Params:
-            mID: the id of the multimedia post in question.
+            hdID: the id of the AboutUs record in question.
 
         Returns:
-            True if the multimedia post exists, false otherwise.
+            True if the AboutUs record exists, false otherwise.
         """
         
         cursor = self.conn.cursor()
