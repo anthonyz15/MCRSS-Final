cfd8ec11f01ca7a0b1525deaf33eb6cc771d1e27
Jean C. Merced <jean.merced2@upr.edu>
2020-12-04 12:42:40 -0400

    Update mutations.js

--- LokiDashboard/store/multimedias/mutations.js
+++ LokiDashboard/store/multimedias/mutations.js
@@ -1,9 +1,8 @@
 /**
- * Vuex store for multimedias, with actions, mutations, getters and state.
+ * Vuex store for multimedias, with actions, mutations, getters, and state.
  * @module multimedias
  */
 
-//These mutations allow you to modify the state of the application.
 export default{
 
     /**
@@ -12,7 +11,6 @@ export default{
      * @param {*} multimedia loaded multimedia post object with data
      */
     SET_MULTIMEDIA(state,multimedia){
-        //Set multimedia post data
         state.multimedia = multimedia
     },
 
@@ -23,7 +21,6 @@ export default{
      */
 
     SET_MULTIMEDIAS(state,multimedias){
-        //Set loaded multimedia posts list
         state.multimedias = multimedias
     },
 
@@ -48,12 +45,11 @@ export default{
     /**
      * Mutation to set the information of the updated multimedia post in the state's multimedia posts list.
      * @param {*} state vuex state object
-     * @param {*} multimedia multimedia post Object with the information of the multimedia post being updated
+     * @param {*} multimedia Object with the information of the multimedia post being updated
      */
     UPDATE_MULTIMEDIA(state,multimedia){
         const index = state.multimedias.findIndex(arrmultimedia => arrmultimedia.mid === multimedia.mid)
         if(index !== -1){
-            //Substitute the old multimedia post with the update multimedia post
             state.multimedias.splice(index,1,multimedia)
         }
     }
